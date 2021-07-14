d2d9137a893db8a5e7b2ddb0bcf16efd2ee444e1
Aydrian Howard <aydrian@gmail.com>
2021-07-13 20:26:35 -0400

    reward overlay clean up

--- pages/overlays.js
+++ pages/overlays.js
@@ -6,6 +6,27 @@ import { EmoteDrop } from "@components/EmoteDrop";
 
 const REWARD_BOOP_ATTICUS = "644b10b6-92ac-4f59-8baa-21c3b3cae5cb";
 
+function AtticusBoop({ event }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "450px",
+        left: "25px",
+        textAlign: "center"
+      }}
+    >
+      <audio autoPlay>
+        <source src="/t-rex-roar.mp3" type="audio/mp3" />
+      </audio>
+      <img src="atticus.gif" width="200px" />
+      <p style={{ fontWeight: "bold", textAlign: "center" }}>
+        {event?.user_name} booped Atticus!
+      </p>
+    </div>
+  );
+}
+
 export default function Overlays() {
   const [reward, setReward] = useState({});
 
@@ -17,7 +38,6 @@ export default function Overlays() {
 
     const channel = pusher.subscribe("itsaydrian-stream");
     channel.bind("redeem-channelpoints", function (data) {
-      console.log("event data: ", data);
       if (data.event?.reward?.id === REWARD_BOOP_ATTICUS) {
         setReward(data);
         setTimeout(() => {
@@ -34,23 +54,7 @@ export default function Overlays() {
   return (
     <ChatProvider channels={["itsaydrian"]}>
       <EmoteDrop filter={["CorgiDerp", "DoritosChip"]} />
-      <div>
-        {reward.event && (
-          <div
-            style={{
-              position: "absolute",
-              top: "450px",
-              left: "25px",
-              textAlign: "center"
-            }}
-          >
-            <img src="atticus.gif" width="200px" />
-            <p style={{ fontWeight: "bold", textAlign: "center" }}>
-              {reward.event?.user_name} booped Atticus!
-            </p>
-          </div>
-        )}
-      </div>
+      <div>{reward.event && <AtticusBoop event={reward.event} />}</div>
       <div
         style={{
           position: "absolute",

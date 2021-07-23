d64d42f3ace35d81b99d28eb9bcf5d349c728a6b
Aydrian Howard <aydrian@gmail.com>
2021-07-23 17:17:42 -0400

    all but chat message styling

---
+++ pages/chat.js
@@ -0,0 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import { ChatProvider } from "@context/chat";
+import { Chat } from "@components/Chat";
+
+export default function ChatPage() {
+  return (
+    <ChatProvider channels={["itsaydrian"]}>
+      <Chat />
+      <ToastContainer closeButton={false} position="bottom-left" />
+    </ChatProvider>
+  );
+}

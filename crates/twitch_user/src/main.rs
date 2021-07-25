0d4c29d3ff938af819171a3bbd440520f7fbd785
Aydrian Howard <aydrian@gmail.com>
2021-07-25 15:40:11 -0400

    returning all the same time. cargo check successful

--- crates/twitch_user/src/main.rs
+++ crates/twitch_user/src/main.rs
@@ -1,6 +1,6 @@
 use lamedh_http::{
     http::StatusCode,
-    lambda::{lambda, Context},
+    lambda::{lambda, Context, Error},
     IntoResponse, Request, RequestExt, Response,
 };
 use reqwest;
@@ -10,18 +10,16 @@ use twitch_oauth2::{
     client::reqwest_http_client, tokens::errors::TokenError, AppAccessToken, ClientId, ClientSecret,
 };
 
-type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
-
 #[lambda(http)]
 #[tokio::main]
 async fn main(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
     // If no query param was provided, return a 400
     let user_id = match request.query_string_parameters().get("user_id") {
-        Some(val) => val,
+        Some(val) => val.to_string(),
         None => {
             return Ok(Response::builder()
                 .status(StatusCode::BAD_REQUEST)
-                .body(())
+                .body(json!({"message": "user_id query string parameter is required"}).to_string())
                 .unwrap());
         }
     };
@@ -46,22 +44,28 @@ async fn main(request: Request, _: Context) -> Result<impl IntoResponse, Error> 
     {
         Ok(t) => t,
         Err(TokenError::Request(e)) => panic!("got error: {:?}", e),
-        Err(e) => panic!(e),
+        Err(e) => panic!("{:?}", e),
     };
 
     let helix: HelixClient<'static, reqwest::Client> = HelixClient::default();
     // better error handling rather than unwrap?
     match helix.get_user_from_id(user_id, &token).await.unwrap() {
-        Some(user) => Ok(json!({
-            "id": user.id,
-            "name": user.display_name,
-            "displayName": user.display_name,
-            "description": user.description,
-            "profilePictureUrl": user.profile_image_url
-        })),
+        Some(user) => Ok(Response::builder()
+            .status(StatusCode::OK)
+            .body(
+                json!({
+                    "id": user.id,
+                    "name": user.display_name,
+                    "displayName": user.display_name,
+                    "description": user.description,
+                    "profilePictureUrl": user.profile_image_url
+                })
+                .to_string(),
+            )
+            .unwrap()),
         None => Ok(Response::builder()
             .status(StatusCode::NOT_FOUND)
-            .body(())
+            .body(json!({"message": "user not found"}).to_string())
             .unwrap()),
     }
 }
